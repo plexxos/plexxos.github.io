5aec6b32051dd8d1a7b1953b3286214f8e1a7ffe
DaveBaan <davebaan@gmail.com>
2022-10-17 17:39:50 +0100

    Completed landing page, ready for deploy

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -4,19 +4,20 @@ import "./Body.css";
 
 const Body = () => {
   return (
-    <section className="px-6 mt-[39px]">
-      <div className="w-full body rounded-t-[60px] h-[576px] px-[41px] pt-[55px]">
-        <div className="w-full h-[306px]">
-          <div className="font-[Ubuntu] text-white text-4xl leading-[54px] font-bold">
+    <section className="px-6 mt-[39px] h-auto sm:mt-[139px] lg:px-10 lg:w-[100vw] lg:overflow-x-clip body">
+      <div className="w-full gradient-container rounded-t-[60px] h-[576px] px-[20px] pt-[55px] md:px-[40px]">
+        <div className="h-[306px]">
+          <div className="text-white text-4xl leading-[50px] font-bold sm:text-[56px] sm:leading-[120%] md:w-[562px]">
             Connect your healthcare team
           </div>
-          <div className="w-[230px]">
-            <div className="font-[Ubuntu] text-white text-xl leading-[30px] font-normal pt-[12px] pb-[36px]">
+          <div className="w-[90%]">
+            <div className="font-[Ubuntu] text-white text-xl leading-[30px] font-normal pt-[12px] pb-[36px] sm:text-[32px] sm:leading-[120%] lg:w-[435px]">
               The all-in-one tool for secure collaboration and effective time
               management.
             </div>
+
             <a href="https://google.com">
-              <div className="font-[Ubuntu] text-base leading-[24px] font-medium bg-white py-[18px] w-[90%] text-center rounded-[20px]">
+              <div className="text-base leading-[24px] font-medium bg-white py-[18px] w-[80%] text-center rounded-[20px] sm:text-[22px] sm:leading-[120%] sm:w-[264px]">
                 Join our Waitlist
               </div>
             </a>
@@ -26,7 +27,7 @@ const Body = () => {
       <img
         src={mobileScreens}
         alt="mobileScreens"
-        className="absolute top-[497px] w-[293.64] h-[400.1px]"
+        className="absolute top-[529px] w-[293.64] h-[400.1px] sm:w-[692px] sm:h-[843px] sm:left-[10%] lg:top-[90px] lg:left-[47%] lg:w-[692px] lg:h-[843px] xl:left-[55%] sm-screen"
       />
     </section>
   );
--- src/components/LogoBar/LogoBar.js
+++ src/components/LogoBar/LogoBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../../public/Plexxos-img/Plexxos-color.png";
 const LogoBar = () => {
   return (
-    <div className="w-full h-20 align-content">
+    <div className="w-full h-20 bg-white align-content sm:fixed sm:top-0 z-10 lg:fixed">
       <img
         src={logo}
         alt="logo"
