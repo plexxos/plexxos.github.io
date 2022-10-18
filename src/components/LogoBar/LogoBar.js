4ae8081673278f3b6ee00853ce469879388e0df2
DaveBaan <davebaan@gmail.com>
2022-10-18 11:24:26 +0100

    minor bug fix

--- src/components/LogoBar/LogoBar.js
+++ src/components/LogoBar/LogoBar.js
@@ -1,16 +1,18 @@
 import React from "react";
 import logo from "../../public/Plexxos-img/Plexxos-color.png";
-import { Link } from "react-router-dom";
 const LogoBar = () => {
   return (
-    <div className="w-full h-20 bg-white align-content sm:fixed sm:top-0 z-10 lg:fixed">
-      <Link to="#">
+    <div
+      className="w-full h-20 bg-white align-content sm:fixed sm:top-0 z-10 lg:fixed"
+      id="logo"
+    >
+      <a href="">
         <img
           src={logo}
           alt="logo"
           className="w-36 align-content mx-auto mt-[9px]"
         />
-      </Link>
+      </a>
     </div>
   );
 };
